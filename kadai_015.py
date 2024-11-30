a24019a24a7514c478c0e3937b219301f80675e6
atsuya03 <147512419+atsuya03@users.noreply.github.com>
2024-12-01 01:48:26 +0900

    Update kadai_015.py

--- kadai_015.py
+++ kadai_015.py
@@ -1,13 +1,11 @@
 class Human:
-    def __init__(self):
-        self.name=""
-        self.age=""
-
-    def printinfo(self,name,age):
+    def __init__(self,name,age):
         self.name=name
         self.age=age
+
+    def printinfo(self):
         print(f"Name: {self.name}, Age: {self.age}")
 
 
-person = Human()
-person.printinfo("侍", 23)
+person = Human("侍",23)
+person.printinfo()
